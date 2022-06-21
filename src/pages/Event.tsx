1f51660610e9b3f351d80b81688071ff5d74431b
Henrique de Rodrigues <tec.henriquedepaula@gmail.com>
2022-06-21 19:55:31 -0300

    Adicionando o component Header do projeto

---
+++ src/pages/Event.tsx
@@ -0,0 +1,13 @@
+import { Header } from "../components/Header";
+function Event() {
+    return (  
+        <>
+        <Header/>
+        <h1>
+            Event
+        </h1>
+        </>
+    );
+}
+
+export default Event;
