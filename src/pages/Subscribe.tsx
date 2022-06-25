1b63a44931ad4d357bf5dced0a148f5dab82816b
Henrique de Rodrigues <tec.henriquedepaula@gmail.com>
2022-06-25 13:39:07 -0300

    Adicionando estilização da página subscribe

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -1,9 +1,18 @@
+import { Fragment } from "react";
 import ButtonSuccess from "../components/ButtonSuccess";
+import Logo from "../components/Logo";
 function Subscribe() {
     return (
-        <main className="flex flex-1 min-w-screen">
-            
-            <form action="/sub" method="POST" className="bg-gray-700 p-8 max-w-[27vw] border border-gray-600" id="subscribe">
+        <Fragment>
+            <div className="flex flex-col items-center min-w-screen bg-blur bg-cover bg-no-repeat">
+            <main className="flex w-full max-w-[1180px] mt-20 mx-28 gap-20 justify-between">
+            <header className="mt-10">
+                <Logo height="30px"width="300px" />
+                <h1 className="mt-8 text-3xl">
+                    Construa uma <strong className="text-blue-500">aplicação completa</strong>,<br /> do zero, com <strong className="text-blue-500">ReactJS</strong>
+                </h1>  
+            </header>   
+            <div id="formulario" className=""><form action="/sub" method="POST" className="bg-gray-700 p-8 max-w-[27vw] border border-gray-600" id="subscribe">
                 <div className="mb-6 flex">
                     <label htmlFor="subscribe" className="text-2xl font-bold ">Inscreva-se Gratuitamente</label>
                 </div>
@@ -25,7 +34,13 @@ function Subscribe() {
                         <ButtonSuccess type="submit" textButton="Garantir minha vaga" />
                     </div>
             </form>
-        </main>
+                </div>
+            </main>
+                <div>
+                    <img src="/src/assets/react-bg.png" alt="react"/>
+                </div>
+            </div>
+        </Fragment>
     );
 }
 
