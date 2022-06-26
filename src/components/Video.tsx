d4f39d6a6a6c2cd89532a9e0de0d73092af1916f
Henrique de Paula Rodrigues <tec.henriquedepaula@gmail.com>
2022-06-26 09:47:15 -0300

    Feature codegen config (#19)
    * Adicionando arquivo de configuração
    
    * Criando os arquivos .graphql das queries/mutations e inserindo-os na pasta /graphql
    
    * Efetuando alterações nos componentes com base no arquivo generated.ts
    
    * Adicionando queries graphql e gerando arquivo generated.ts através do plugin @graphql-codegen/cli
    
    * Atualizando página subscriber e adicionando script de configuração do codegen

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,121 +1,93 @@
-import {CaretRight, DiscordLogo, FileArrowDown, FileImage, Lightning } from "phosphor-react";
+import { CaretRight, DiscordLogo, FileArrowDown, FileImage, Lightning } from "phosphor-react";
 import ButtonSuccess from "./ButtonSuccess";
 import ButtonSecondary from "./ButtonSecondary";
 import { Player, Youtube } from "@vime/react";
-import { gql, useQuery } from "@apollo/client";
-interface IVideoProps{
+import { useGetLessonBySlugQuery } from "../graphql/generated";
+interface IVideoProps {
   lessonSlug: string;
 }
-interface ILessonProps{
-  lesson: {
-    id: string;
-    title: string;
-    description: string;
-    videoId: string;
-    teacher: {
-      name: string;
-      bio: string;
-      avatarURL: string;
-    }
-  }
-}
-const GET_LESSON_BY_SLUG = gql`
-query getLessonBySlug($slug: String) {
-  lesson(where: {slug: $slug}) {
-    id
-    title
-    videoId
-    description
-    teacher {
-      avatarURL
-      bio
-      name
-    }
-    
-  }
-}
-`
 export function Video(props: IVideoProps) {
-  const { data, loading } = useQuery<ILessonProps>(GET_LESSON_BY_SLUG, {
+  const { data, loading } = useGetLessonBySlugQuery({
     fetchPolicy: 'network-only',
     variables: { slug: props.lessonSlug },
   });
-  if (loading||!data) {
+  if (loading || !data || !data.lesson) {
     return (
       <div className="flex-1">
         <p> Aqui virá o componente de loading</p>
       </div>
     )
   }
-return (
-      <div className="flex-1">
-        <div className="bg-black flex justify-center">
-          <div className="h-[90%] w-[90%] aspect-video max-w[1180px] max-h-[60vh]">
-            <Player controls={true}>
-              <Youtube videoId={data?.lesson.videoId}/>
-            </Player></div>
-        </div>
-        <div className="p-8 mx-auto max-w-[1180px]">
-          <div className="flex items-start gap-16">
-            <div className="flex-1">
-              <h1 className="text-2xl font-bold">
-                {data?.lesson.title}
+  return (
+    <div className="flex-1">
+      <div className="bg-black flex justify-center">
+        <div className="h-[90%] w-[90%] aspect-video max-w[1180px] max-h-[60vh]">
+          <Player controls={true}>
+            <Youtube videoId={data.lesson.videoId} />
+          </Player></div>
+      </div>
+      <div className="p-8 mx-auto max-w-[1180px]">
+        <div className="flex items-start gap-16">
+          <div className="flex-1">
+            <h1 className="text-2xl font-bold">
+              {data?.lesson.title}
             </h1>
-            <p className="mt-4 text-gray-200 leading-relaxed">{ data?.lesson.description}</p>
+            <p className="mt-4 text-gray-200 leading-relaxed">{data?.lesson.description}</p>
+          </div>
+          <div className="flex flex-col gap-4">
+            <ButtonSuccess
+              textButton="Comunidade do Discord"
+              href="https://discord.gg/yWKJfrnK"
+              icon={
+                <DiscordLogo size={24}></DiscordLogo>}
+            ></ButtonSuccess>
+            <ButtonSecondary
+              textButton=" Seguir para desafios"
+              href="#"
+              icon={<Lightning size={24}></Lightning>}
+            ></ButtonSecondary>
+          </div>
+        </div>
+        <div className="flex flex-1 mt-6 gap-4">
+          {data.lesson.teacher &&
+            <img
+            src={data?.lesson.teacher.avatarURL}
+            alt="Teacher Profile Icon"
+            className="rounded-full border-2 border-blue-500 max-h-[8vh] max-w-[8vh]"
+          />}
+          <div className="leading-relaxed">
+            <strong className="text-bold text-2xl block">{data?.lesson.teacher.name}</strong>
+            <span className="text-sm text-gray-200 block">{data?.lesson.teacher.bio}</span>
+          </div>
+        </div>
+        <div className="mt-20 grid gap-8 items-stretch grid-cols-2">
+
+
+          <a href="" className="bg-gray-700 flex justify-start rounded overflow-hidden gap-6 hover:bg-gray-600">
+            <div className="bg-green-700 flex items-center p-6 h-full"><FileArrowDown size={40} /></div>
+            <div className="py-6 leading-relaxed">
+              <strong className="text-2xl"> Material Complementar</strong>
+              <p className="text-sm text-gray-200 mt-2 ">Acesse o material complementar para acelerar o seu desenvolvimento</p>
             </div>
-            <div className="flex flex-col gap-4">
-              <ButtonSuccess
-                textButton="Comunidade do Discord"
-                href="https://discord.gg/yWKJfrnK"
-                icon={
-                  <DiscordLogo size={24}></DiscordLogo>}
-              ></ButtonSuccess>
-              <ButtonSecondary
-                textButton=" Seguir para desafios"
-                href="#"
-                icon={<Lightning size={24}></Lightning>}
-              ></ButtonSecondary>
+            <div className="h-full flex items-center p-6">
+              <CaretRight className="text-blue-300" size={24} />
             </div>
-          </div>
-            <div className="flex flex-1 mt-6 gap-4">
-              <img
-                src={data?.lesson.teacher.avatarURL}
-                alt="Teacher Profile Icon"
-                className="rounded-full border-2 border-blue-500 max-h-[8vh] max-w-[8vh]"
-              />
-              <div className="leading-relaxed">
-                <strong className="text-bold text-2xl block">{data?.lesson.teacher.name}</strong>
-                <span className="text-sm text-gray-200 block">{data?.lesson.teacher.bio}</span>
-              </div>
-          </div>
-          <div className="mt-20 grid gap-8 items-stretch grid-cols-2">
-            
-            
-            <a href="" className="bg-gray-700 flex justify-start rounded overflow-hidden gap-6 hover:bg-gray-600">
-              <div className="bg-green-700 flex items-center p-6 h-full"><FileArrowDown size={40} /></div>
-              <div className="py-6 leading-relaxed">
-                <strong className="text-2xl"> Material Complementar</strong>
-                <p className="text-sm text-gray-200 mt-2 ">Acesse o material complementar para acelerar o seu desenvolvimento</p>
-              </div>
-              <div className="h-full flex items-center p-6">
-              <CaretRight className="text-blue-300" size={24}/>
-              </div>
-            </a>
-            
-            
-            <a href="" className="bg-gray-700 flex  justify-start rounded overflow-hidden gap-6 hover:bg-gray-600">
-              <div className="bg-green-700 flex items-center p-6 h-full"><FileImage size={40} /></div>
-              <div className="py-6 leading-relaxed">
-                <strong className="text-2xl"> Material Complementar</strong>
-                <p className="text-sm text-gray-200 mt-2">Acesse o material complementar para acelerar o seu desenvolvimento</p>
-              </div>
-              <div className="h-full flex items-center p-6">
-              <CaretRight className="text-blue-300" size={24}/>
-              </div>
-            </a>
-            
-          </div>
+          </a>
+
+
+          <a href="" className="bg-gray-700 flex  justify-start rounded overflow-hidden gap-6 hover:bg-gray-600">
+            <div className="bg-green-700 flex items-center p-6 h-full"><FileImage size={40} /></div>
+            <div className="py-6 leading-relaxed">
+              <strong className="text-2xl"> Material Complementar</strong>
+              <p className="text-sm text-gray-200 mt-2">Acesse o material complementar para acelerar o seu desenvolvimento</p>
+            </div>
+            <div className="h-full flex items-center p-6">
+              <CaretRight className="text-blue-300" size={24} />
+            </div>
+          </a>
+
         </div>
       </div>
-    )
+    </div>
+  )
 }
